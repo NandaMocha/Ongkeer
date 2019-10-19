f40e281b26ec20addc16df9855f12aa117b545f5
nandamocha <nanda.mocha@gmail.com>
2019-10-19 11:20:18 +0700

    Initial Commit

---
+++ OngkeerMVC/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OngkeerMVC
+//
+//  Created by Nanda Mochammad on 19/10/19.
+//  Copyright © 2019 Nanda Mochammad. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
